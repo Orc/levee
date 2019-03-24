0c4008f67b053012f8879e72f76c4729900bec4b
david parsons <orc@pell.portland.or.us>
2019-03-24 10:45:53 -0700

    add the tinned windows configuration

---
+++ win32/config.h
@@ -0,0 +1,27 @@
+/*
+ * configuration for levee, generated Wed Sep 26 03:36:32 UTC 2018
+ * by orc@tinyd.pell.portland.or.us
+ */
+#ifndef __AC_LEVEE_D
+#define __AC_LEVEE_D 1
+
+#include <stdlib.h>
+
+
+#define OS_WINDOWS 1
+#define os_pid_t pid_t
+#define USING_STDIO 1
+#define LOGGING 1
+#define EDITSIZE 256000
+#define HAVE_STRING_H 1
+#define HAVE_MEMSET 1
+#define HAVE_STRCHR 1
+#define HAVE_SIGNAL_H 1
+#define HAVE_SIGNAL 1
+#define HAVE_TGETENT 1
+#define HAVE_TERMCAP_H 1
+#define USE_TERMCAP 1
+#define GLOB_REQUIRED 1
+#define HAVE_STRDUP 1
+
+#endif/* __AC_LEVEE_D */
